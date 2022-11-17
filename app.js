3c5ccf31d10b9a77d09b2d6fbebe7ea243cea740
Meraj Kazi <kazimeraj11@gmail.com>
2022-11-17 18:20:10 +0600

    Facebook auth added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-require('./auth');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
 
 const app = express();
 
@@ -9,37 +9,62 @@ function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('<a href="/auth/google">Authenticate with Google</a>');
+app.get("/", (req, res) => {
+  res.send(
+    '<a href="/auth/google">Authenticate with Google</a> <br> <a href="/auth/facebook">Authenticate with Facebook</a>'
+  );
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
-));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-app.get( '/auth/google/callback',
-  passport.authenticate( 'google', {
-    successRedirect: '/protected',
-    failureRedirect: '/auth/google/failure'
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/google/failure",
   })
 );
 
-app.get('/protected', isLoggedIn, (req, res) => {
+app.get("/protected", isLoggedIn, (req, res) => {
   res.send(`Hello ${req.user.displayName}`);
 });
 
-app.get('/logout', (req, res) => {
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
+
+app.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/facebook/protected",
+    failureRedirect: "/auth/facebook/failure",
+  })
+);
+
+app.get("/facebook/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get("/auth/facebook/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
+
+app.get("/logout", (req, res) => {
   req.logout();
   req.session.destroy();
-  res.send('Goodbye!');
+  res.send("Goodbye!");
 });
 
-app.get('/auth/google/failure', (req, res) => {
-  res.send('Failed to authenticate..');
-});
-
-app.listen(5000, () => console.log('listening on port: 5000'));
+app.listen(5000, () => console.log("listening on port: 5000"));
--- auth.js
+++ auth.js
@@ -1,24 +1,45 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-require('dotenv').config();
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
-  passReqToCallback: true,
-},
-function(request, accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}));
+const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
-passport.serializeUser(function(user, done) {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:5000/auth/facebook/callback",
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
